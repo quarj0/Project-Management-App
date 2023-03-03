df48509aa4308574c1e41dbdaabdf4ffcadd7f95
Owusu Ansah Kwadwo <54241472+hacks-and-codes@users.noreply.github.com>
2023-03-03 19:38:01 +0000

    adding delete task button

--- projectmanagent.py
+++ projectmanagent.py
@@ -4,9 +4,10 @@ from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from tkinter import messagebox
+import os
 
 # Create a data frame to store project tasks
-tasks_df = pd.DataFrame(columns=['Task Name', 'Task Description', 'Start Date', 'End Date', 'Assigned To', 'Status'])
+tasks_df =pd.read_csv('tasks.csv') if os.path.exists('tasks.csv') else pd.DataFrame(columns=['Task Name', 'Task Description', 'Start Date', 'End Date', 'Assigned To', 'Status'])
 
 # Create a function to add new tasks to the data frame
 def add_task():
@@ -21,9 +22,9 @@ def add_task():
     if task_name == '' or task_description == '' or start_date == '' or end_date == '' or assigned_to == '' or status == '':
         messagebox.showerror('Please fill all the fields to add task.')
         
-    
     global tasks_df
     tasks_df = pd.concat([tasks_df, new_task], ignore_index=True)
+    tasks_df.to_csv('tasks.csv', index=False) # save the tasks to a CSV file
     update_table()
     task_name_entry.delete(0, tk.END)
     task_description_entry.delete(0, tk.END)
@@ -38,6 +39,7 @@ def update_status():
     if selected_task:
         task_index = int(selected_task[0])-1
         tasks_df.at[task_index, 'Status'] = status_entry.get()
+        tasks_df.to_csv('tasks.csv', index=False) # save the tasks to a CSV file
         update_table()
 
 # Create a function to update the tasks table
@@ -45,10 +47,20 @@ def update_table():
     tasks_table.delete(*tasks_table.get_children())
     for i, row in tasks_df.iterrows():
         tasks_table.insert("", tk.END, values=(i+1, row['Task Name'], row['Task Description'], row['Start Date'], row['End Date'], row['Assigned To'], row['Status']))
+        delete_button = tk.Button(tasks_table, text='Delete', command=lambda i=i: delete_task(i))
+        tasks_table.create_window(tasks_table.index(0, 0, root=delete_button, padx=2, pady=2))
+
+def delete_task(index):
+    global tasks_df
+    tasks_df = tasks_df.drop(index=index)
+    tasks_df.reset_index(drop=True, inplace=True)
+    update_table()
 
 # Create a GUI for the project management app
 root = tk.Tk()
 root.title("Project Management App")
+root.configure(background="#ccc")
+
 
 # Create input fields for new tasks
 task_name_label = tk.Label(root, text="Task Name:")
@@ -68,7 +80,7 @@ status_entry = tk.Entry(root)
 add_task_button = tk.Button(root, text="Add Task", command=add_task)
 
 # Create a table to display project tasks
-tasks_table = tk.ttk.Treeview(root, columns=(1, 2, 3, 4, 5, 6, 7), show="headings", height=10)
+tasks_table = tk.ttk.Treeview(root, columns=(1, 2, 3, 4, 5, 6, 7), show="headings", height=10,)
 tasks_table.heading(1, text="ID")
 tasks_table.heading(2, text="Task Name")
 tasks_table.heading(3, text="Task Description")
@@ -113,7 +125,9 @@ status_entry.grid(row=5, column=1)
 add_task_button.grid(row=6, column=1)
 tasks_table.grid(row=7, columnspan=2)
 update_status_button.grid(row=8, column=1)
+# delete_button.grid(row=8, column=2)
 sort_by_start_date_button.grid(row=8, column=0)
 canvas.get_tk_widget().grid(row=9, columnspan=2)
+
 
 root.mainloop()
